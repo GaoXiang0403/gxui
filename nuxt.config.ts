feecfe67ca805c28c675f5ca4052b80e68a9f5f6
xiang.gao <57524479+GaoXiang0403@users.noreply.github.com>
2024-12-03 21:46:44 +0800

    修改项目部署路径

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,7 +2,7 @@
  * @Author: xiang.gao 57524479+GaoXiang0403@users.noreply.github.com
  * @Date: 2024-11-21 00:44:22
  * @LastEditors: xiang.gao 57524479+GaoXiang0403@users.noreply.github.com
- * @LastEditTime: 2024-12-03 21:39:16
+ * @LastEditTime: 2024-12-03 21:46:32
  * @FilePath: /gxdemo/nuxt.config.ts
  * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
  */
@@ -14,7 +14,7 @@ export default defineNuxtConfig({
   },
   css: ["~/assets/scss/common.scss"],
   app: {
-    // baseURL: "/gxui/",
+    baseURL: "/gxui/",
     head: {
       titleTemplate: "%s %separator %siteName",
       templateParams: {
