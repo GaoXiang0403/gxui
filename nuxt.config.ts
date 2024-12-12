bb1d5765dc150f226c29002750892df6157ec4dd
xiang.gao <57524479+GaoXiang0403@users.noreply.github.com>
2024-12-12 19:50:02 +0800

    配置文件增加

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,7 +2,7 @@
  * @Author: xiang.gao 57524479+GaoXiang0403@users.noreply.github.com
  * @Date: 2024-11-21 00:44:22
  * @LastEditors: xiang.gao 57524479+GaoXiang0403@users.noreply.github.com
- * @LastEditTime: 2024-12-12 19:40:17
+ * @LastEditTime: 2024-12-12 19:48:10
  * @FilePath: /gxdemo/nuxt.config.ts
  * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
  */
@@ -14,6 +14,7 @@ export default defineNuxtConfig({
   },
   css: ["~/assets/scss/common.scss"],
   app: {
+    // baseURL: "/gxui/",
     head: {
       titleTemplate: "%s %separator %siteName",
       templateParams: {
